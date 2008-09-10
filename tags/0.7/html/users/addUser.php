a493630ea995f5eaf6b5bff78d5c97efd7f013ec
inghamn <inghamn@9afe1303-3308-a59e-47b3-9f8ac9fa10f0>
2008-09-10 14:04:20 +0000

    Created a tag of the original version, created by Ben Dunn

---
+++ tags/0.7/html/users/addUser.php
@@ -0,0 +1,57 @@
+<?php
+/*
+	$_POST variables:	authenticationMethod
+										username
+										roles
+
+						# May be optional if LDAP is used
+						password
+						firstname
+						lastname
+						email
+						homephone
+
+*/
+	verifyUser("Administrator");
+
+	#--------------------------------------------------------------------------
+	# Create the new account
+	#--------------------------------------------------------------------------
+	$user = new User();
+	$user->setAuthenticationMethod($_POST['authenticationMethod']);
+	$user->setUsername($_POST['username']);
+	if ($_POST['password']) { $user->setPassword($_POST['password']); }
+	if (isset($_POST['roles'])) { $user->setRoles($_POST['roles']); }
+
+	if ($_POST['authenticationMethod'] == "LDAP")
+	{
+		# Load the rest of their stuff from LDAP
+		require_once(GLOBAL_INCLUDES."/classes/LDAPEntry.inc");
+		$ldap = new LDAPEntry($user->getUsername());
+		$user->setFirstname($ldap->getFirstname());
+		$user->setLastname($ldap->getLastname());
+		$user->setEmail($ldap->getEmail());
+		$user->setHomePhone($ldap->getHomephone());
+	}
+	else
+	{
+		# For local authentication.
+		$user->setFirstname($_POST['firstname']);
+		$user->setLastname($_POST['lastname']);
+		$user->setEmail($_POST['email']);
+		$user->setHomePhone($_POST['homephone']);
+		if (isset($_POST['photo'])) { $user->setPhotoPath($_POST['photo']); }
+		else { $user->setPhotoPath("http://isotope.bloomington.in.gov/directory/images/nophoto.jpg"); }
+	}
+
+	try
+	{
+		$user->save();
+		Header("Location: home.php");
+	}
+	catch (Exception $e)
+	{
+		$_SESSION['errorMessages'][] = $e;
+		Header("Location: addUserForm.php");
+	}
+?>
---
+++ tags/0.7/html/users/updateUser.php
@@ -0,0 +1,66 @@
+<?php
+/*
+	$_POST variables:	id
+						authenticationMethod
+						username
+						roles
+						firstname
+						lastname
+
+						# Optional
+						password
+						email
+						homephone
+						workphone
+						city
+						address
+						zip
+						picture_url
+*/
+	verifyUser("Administrator", "Committee Member");
+
+	#--------------------------------------------------------------------------
+	# Update the account
+	#--------------------------------------------------------------------------
+	$user = new User($_POST['id']);
+	
+	#Only updated when Admin accesses an account other than their own
+	if ($_POST['username']) { $user->setUsername($_POST['username']); $direct = "home.php";}
+	else { $direct = BASE_URL."/viewProfile.php?id={$_POST['id']}"; }
+	if ($_POST['authenticationMethod']) {$user->setAuthenticationMethod($_POST['authenticationMethod']); }
+	
+	$user->setFirstname($_POST['firstname']);
+	$user->setLastname($_POST['lastname']);
+	$user->setEmail($_POST['email']);
+	$user->setHomephone($_POST['homephone']);
+	$user->setWorkphone($_POST['workphone']);
+	$user->setCity($_POST['city']);
+	$user->setAddress($_POST['address']);
+	$user->setZipCode($_POST['zip']);
+	$user->setAbout($_POST['about']);
+	$user->setTimestamp(date("Y-m-d H:i:s",time()));	
+
+	# Only update the password if they actually typed somethign in
+	if ($_POST['password']) { $user->setPassword($_POST['password']); }
+	if (isset($_POST['roles'])) { $user->setRoles($_POST['roles']); }
+	
+	# Only update the photoPath if they supplied a url
+	if (isset($_POST['picture_url'])) 
+	{
+		$file = basename($_POST['picture_url']);
+		$file = explode(".", $file);
+		$ext = $file[1];
+		if ($ext == "jpg") { $user->setPhotoPath($_POST['picture_url']); }
+	}
+
+	try
+	{
+		$user->save();
+		Header("Location: ". $direct);
+	}
+	catch (Exception $e)
+	{
+		$_SESSION['errorMessages'][] = $e;
+		Header("Location: updateUserForm.php?id={$user->getId()}");
+	}
+?>
